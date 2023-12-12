0dbf6d4bfbb6315356bfa33224b50002acaf711e
vuongnhattin <131172061+vuongnhattin@users.noreply.github.com>
2023-12-12 14:23:13 +0700

    update server's UI.h

--- Server/UI.h
+++ Server/UI.h
@@ -2,18 +2,26 @@
 #include <WinSock2.h>
 
 enum class State {
-	CONNECT_MENU,
-	SENDING_CONTENT,
+	DISPLAY_CONNECTION_MENU,
+	START_THREADS,
+	DISPLAY_CONTROL_PANEL,
+	QUIT,
+};
+
+enum class BindingState {
+	BINDING,
+	FAILED,
+	BOUND,
 };
 
 enum class ConnectionState {
-	BINDING,
-	BIND_FAILED,
-	BOUND,
-	CONNECTING,
+	NOT_YET,
+	WAITING,
 	CONNECTED,
 };
 
 void initUI();
 void freeUI();
-void displayConnectPanel();
+void displayConnectMenu();
+void waitingForConnection(SOCKET& serverSocket, SOCKET& acceptSocket);
+void displayControlPanel();
